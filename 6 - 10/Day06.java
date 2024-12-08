0e849e40473fa6bfa2f33c0e1f0881eaf3c6cce6
naviddewman <navid.dewman@gmail.com>
2024-12-08 18:48:30 +1100

    added day06 part 2

--- 6 - 10/Day06.java
+++ 6 - 10/Day06.java
@@ -10,11 +10,13 @@ public class Day06 {
     final int[] RIGHT = {0,1};
 
     char[][] map;
-    int distinctPositions;
+    int[] initPos;
+    int distinctPositions, obstaclePositions;
 
     public Day06() {
         map = new char[SIZE][SIZE];
         distinctPositions = 0;
+        obstaclePositions = 0;
     }
 
     public static void main(String[] args) {
@@ -25,16 +27,17 @@ public class Day06 {
     public void run() {
         populateMap();
         // part 1
-        // runSimulation();
-        // calculatePositions();
+        runSimulation();
+        calculatePositions();
         // part 2
         obstaclePlacer();
-        System.out.println(distinctPositions);
+        //System.out.println(distinctPositions);
+        System.out.println(obstaclePositions);
     }
 
     public void populateMap() {
         try (
-            BufferedReader br = new BufferedReader(new FileReader("../dat/aoc06.txt"));
+            BufferedReader br = new BufferedReader(new FileReader("./dat/aoc06.txt"));
         ) {
             String line;
             int row = 0;
@@ -47,12 +50,44 @@ public class Day06 {
     }
 
     public void obstaclePlacer() {
-        
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                int[] pos = findPos();
+                if (map[i][j] != '#') {
+                    if (i == pos[0] && j == pos[1]) { continue; }
+                    map[i][j] = 'O';
+                    if (checkObstaclePos()) { obstaclePositions++; }
+                    map[i][j] = '.';
+                }
+            }
+        }
+    }
+
+    public boolean checkObstaclePos() {
+        int[] direction = UP;
+        int[] pos = initPos;
+        int obstacleEncountered = 0;
+        int loop = 0;
+        while(obstacleEncountered < 10 && loop < 10000) {
+            loop++;
+            int[] nextPos = {pos[0] + direction[0], pos[1] + direction[1]};
+            if (!positionValid(nextPos)) { return false; } // next position out of map bound, poor obstacle placement
+            if (checkForObstacle(nextPos)) { obstacleEncountered++; } 
+            if (checkForObstruction(nextPos)) {
+                if      (direction == UP) { direction = RIGHT; }
+                else if (direction == RIGHT) { direction = DOWN; }
+                else if (direction == DOWN) { direction = LEFT; }
+                else if (direction == LEFT) { direction = UP; }
+            }
+            else { pos = nextPos; }
+        }
+        return true;
     }
 
     public void runSimulation() {
         int[] direction = UP;
         int[] pos = findPos();
+        initPos = pos;
         markPosition(pos);
         
         while (positionValid(pos)) {
@@ -77,10 +112,16 @@ public class Day06 {
     public boolean checkForObstruction(int[] pos) {
         int row = pos[0];
         int col = pos[1];
-        return map[row][col] == '#';
+        return map[row][col] == '#' || map[row][col] == 'O';
     }
 
-    public boolean positionValid(int[] pos) { return pos[0] < SIZE && pos[1] < SIZE; }
+    public boolean checkForObstacle(int[] pos) {
+        int row = pos[0];
+        int col = pos[1];
+        return map[row][col] == 'O';
+    }
+
+    public boolean positionValid(int[] pos) { return pos[0] >= 0 && pos[0] < SIZE && pos[1] >= 0 && pos[1] < SIZE; }
 
     public int[] findPos() {
         for (int i = 0; i < SIZE; i++) {
@@ -97,5 +138,15 @@ public class Day06 {
                 if (map[i][j] == 'X') { distinctPositions++; }
             }
         }
+    }
+
+    public void printMap() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                System.out.print(map[i][j]);
+            }
+            System.out.print("\n");
+        }
+        System.out.print("\n");
     }
 }
